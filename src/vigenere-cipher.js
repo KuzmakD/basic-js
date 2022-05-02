b5c30ae9f56b2406d77314a40ba53a5380c7cab1
Dmitry Korzun <korzundm7@gmail.com>
2022-05-02 19:30:48 +0300

    feat: add vigenere-ciphers task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,17 +19,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
 
-  
-  
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class VigenereCipheringMachine {
+  constructor(direct) {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    this.direct = direct || direct === undefined;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let currentKeyIndex = 0;
+    let result = "";
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i].toUpperCase())) {
+        result += message[i]; continue;
+      }
+      const secretMessage = this.alphabet.indexOf(message[i].toUpperCase());
+      const secretCode = this.alphabet.indexOf(key[currentKeyIndex++ % key.length].toUpperCase());
+      const encryptCode = (secretMessage + secretCode) % this.alphabet.length;
+      result += this.alphabet[encryptCode];
+    }
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    let currentKeyIndex = 0;
+    let result = "";
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i].toUpperCase())) {
+        result += message[i]; continue;
+      }
+
+      const secretMessage = this.alphabet.indexOf(message[i].toUpperCase());
+      const secretCode = this.alphabet.indexOf(key[currentKeyIndex++ % key.length].toUpperCase());
+      const decryptCode = secretMessage - secretCode % this.alphabet.length;
+      if (decryptCode < 0) {
+        result += this.alphabet[this.alphabet.length + decryptCode];
+      } else {
+        result += this.alphabet[decryptCode];
+      }
+    }
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
