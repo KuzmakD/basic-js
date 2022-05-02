0b10dcaad3c9597433552a79bb80f6d66c5df536
Dmitry Korzun <korzundm7@gmail.com>
2022-05-02 13:47:59 +0300

    feat: add tasks recursive, sort-by-height, transform

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 0;
+    arr.forEach(e => {
+      if (Array.isArray(e)) depth = Math.max(depth, this.calculateDepth(e));
+    });
+    return depth + 1;
   }
 }
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const result = [];
+  let k = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0 && (arr[0] === '--discard-prev' || arr[0] === '--double-prev')) continue;
+    if ((i === (arr.length - 1)) && (arr[arr.length - 1] === '--double-next' || arr[arr.length - 1] === '--discard-next')) break;
+    if (arr[i] == `--discard-next`) { k = i + 2; i++; } else
+      if (arr[i] == `--discard-prev`) { (i === k) ? k = i : result.pop(); } else
+        if (arr[i] == `--double-next`) { result.push(arr[i + 1]); } else
+          if (arr[i] == `--double-prev`) {
+            (i === k) ? k = i : result.push(arr[i - 1])
+          }
+          else { result.push(arr[i]); }
+  }
+  return result;
 }
 
 module.exports = {
